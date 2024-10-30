10166d6dc851ad34b52533bae8aeb01fd06ea81b
Park Ju Hyung <qkrwngud825@gmail.com>
2024-10-29 22:48:35 -0400

    anykernel: Install at the inactive slot too
    Because we're shipping KernelSU, this becomes necessary for
    preserving not only the kernel but root across OTA updates.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [@0ctobot: Based on arter97/AnyKernel3@5231aed]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -14,18 +14,32 @@ eval $(cat $AKHOME/props | grep -v '\.')
 
 ### AnyKernel install
 
-# boot install
-BLOCK=boot;
-IS_SLOT_DEVICE=1;
-. tools/ak3-core.sh;
-split_boot;
-flash_boot;
+install() {
+  # boot install
+  BLOCK=boot;
+  IS_SLOT_DEVICE=1;
+  . tools/ak3-core.sh;
+  split_boot;
+  flash_boot;
 
-# vendor_kernel_boot install
-BLOCK=vendor_kernel_boot;
-IS_SLOT_DEVICE=1;
-reset_ak;
-split_boot;
-flash_boot;
+  # vendor_kernel_boot install
+  BLOCK=vendor_kernel_boot;
+  IS_SLOT_DEVICE=1;
+  reset_ak;
+  split_boot;
+  flash_boot;
+}
+
+install;
+
+case $IS_SLOT_DEVICE in
+  1|auto)
+    ui_print " ";
+    ui_print "Installing to inactive slot...";
+    SLOT_SELECT=inactive;
+    unset BLOCK;
+    install;
+  ;;
+esac
 
 ## end install
